1daf34cdbc5406d6a3fd32e9a063208662da5157
Franco <jfranco8@outlook.com>
2022-06-03 15:15:20 -0500

    add read -e -r

--- 03_Win10/GPU_ID.sh
+++ 03_Win10/GPU_ID.sh
@@ -8,7 +8,7 @@ echo "----------------------------------------------------------------"
 #show the PC IDS
 lspci -nn -k | grep -EA3 'VGA|3D|Display|Audio'
 echo "----------------------------------------------------------------"
-read -p "Please paste the GPU ID separated by comma: " gpu_id
+read -e -p "Please paste the GPU ID separated by comma: " -r gpu_id
 echo "options vfio-pci ids=$gpu_id disable_vga=1" > /etc/modprobe.d/vfio.conf
 echo "----------------------------------------------------------------"
 echo "you can check or edit your VFIO ID with the following command:"
